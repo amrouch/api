1d5c397654b019c41de088740bb86c81ffdeeeec
omar hemdane <omar.hemdane@gmail.com>
2023-12-06 21:15:58 +0100

    Reset Passwors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,6 @@
 const User = require("../models/UserModel");
+const Reset = require("../models/ResetModel");
+const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -27,7 +29,7 @@ const login = async (req, res, next) => {
     try {
         const user = await User.findOne({ username: req.body.username });
         if (user) {
-            const pass = bcrypt.compare(req.body.password, user.password);
+            const pass = await bcrypt.compare(req.body.password, user.password);
 
             if (pass) {
                 const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT);
@@ -52,7 +54,57 @@ const login = async (req, res, next) => {
     }
 }
 
+const forgotPass = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const token = crypto.randomBytes(32).toString('hex');
+        const expiresAt = new Date();
+        expiresAt.setHours(expiresAt.getHours() + 1);
+        const resetPassword = new Reset({
+            user: user._id,
+            token,
+            expiresAt,
+        });
+        await resetPassword.save();
+        res.json({ message: 'Reset password token sent successfully' });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const resetPass = async (req, res) => {
+    try {
+        const { token, newPassword } = req.body;
+        const hashedPwd = await bcrypt.hash(newPassword, 10);
+        const resetPassword = await Reset.findOne({ token });
+
+        if (!resetPassword || resetPassword.expires < new Date()) {
+            return res.status(400).json({ message: 'Invalid or expired token' });
+        }
+
+        const user = await User.findById(resetPassword.user);
+        user.password = hashedPwd;
+        await user.save();
+        await Reset.findByIdAndDelete(resetPassword._id);
+        res.json({ message: 'Password reset successfully' });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
     register,
-    login
+    login,
+    forgotPass,
+    resetPass
 }
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -9,4 +9,10 @@ router.post("/register", authController.register)
 //login
 router.post("/login", authController.login)
 
+//forgot password
+router.post('/forgot', authController.forgotPass);
+
+//reset password
+router.post('/reset', authController.resetPass);
+
 module.exports = router;
